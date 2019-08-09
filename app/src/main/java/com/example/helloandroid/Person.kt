ce542b7a0f0ef0168e5a4e100a423f04b35424d5
mbeutel <manfred.beutel@andrena.de>
2019-08-10 01:43:04 +0200

    data binding

---
+++ app/src/main/java/com/example/helloandroid/Person.kt
@@ -0,0 +1,16 @@
+package com.example.helloandroid
+
+import android.view.View
+import androidx.databinding.ObservableField
+
+class Person(var _firstName: String, var _lastName: String) {
+
+    var firstName = ObservableField<String>(_firstName)
+    var lastName = ObservableField<String>(_lastName)
+
+    fun changeName(view: View) {
+        println("i am called")
+        firstName.set("new name")
+        println("persons new first name is ${firstName.get() }")
+    }
+}
--- app/src/main/java/com/example/helloandroid/RandomActivity.kt
+++ app/src/main/java/com/example/helloandroid/RandomActivity.kt
@@ -2,11 +2,16 @@ package com.example.helloandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.helloandroid.databinding.ActivityRandomBinding
 
 class RandomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_random)
+        val binding : ActivityRandomBinding = DataBindingUtil.setContentView<ActivityRandomBinding>(this, R.layout.activity_random)
+        val person: Person = Person("first name", "last name")
+        binding.person = person
     }
 }
